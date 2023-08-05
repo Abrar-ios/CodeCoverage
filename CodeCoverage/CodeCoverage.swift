59b073597e10a9d74fe50628e75ed0a42c7c46c6
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-05 17:11:36 +0500

    Implement static func in production code

--- CodeCoverage/CodeCoverage.swift
+++ CodeCoverage/CodeCoverage.swift
@@ -5,3 +5,15 @@
 //  Created by HAQQQABD on 05/08/2023.
 //
 
+import UIKit
+
+class CodeCoverage {
+    
+    static func max(_ x:Int, _ y:Int) -> Int {
+        if(x < y){
+            return y
+        }else {
+            return x
+        }
+    }
+}
